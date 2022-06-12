c14a549dcecb1630c30d9fe827774dce32fc9d96
Alex Schokking <aschokking@gmail.com>
2022-06-11 20:19:13 -0700

    Remove concept of the editor

--- app.js
+++ app.js
@@ -1,114 +1,18 @@
+const codeObj = {
+    init: function(elevators, floors) {
+        var elevator = elevators[0]; // Let's use the first elevator
 
-var createEditor = function() {
-    var lsKey = "elevatorCrushCode_v5";
-
-    var cm = CodeMirror.fromTextArea(document.getElementById("code"), {
-        lineNumbers: true,
-        indentUnit: 4,
-        indentWithTabs: false,
-        theme: "solarized light",
-        mode: "javascript",
-        autoCloseBrackets: true,
-        extraKeys: {
-            // the following Tab key mapping is from http://codemirror.net/doc/manual.html#keymaps
-            Tab: function(cm) {
-                var spaces = new Array(cm.getOption("indentUnit") + 1).join(" ");
-                cm.replaceSelection(spaces);
-            }
-        }
-    });
-
-    // reindent on paste (adapted from https://github.com/ahuth/brackets-paste-and-indent/blob/master/main.js)
-    cm.on("change", function(codeMirror, change) {
-        if(change.origin !== "paste") {
-            return;
-        }
-
-        var lineFrom = change.from.line;
-        var lineTo = change.from.line + change.text.length;
-
-        function reindentLines(codeMirror, lineFrom, lineTo) {
-            codeMirror.operation(function() {
-                codeMirror.eachLine(lineFrom, lineTo, function(lineHandle) {
-                    codeMirror.indentLine(lineHandle.lineNo(), "smart");
-                });
-            });
-        }
-
-        reindentLines(codeMirror, lineFrom, lineTo);
-    });
-
-    var reset = function() {
-        cm.setValue($("#default-elev-implementation").text().trim());
-    };
-    var saveCode = function() {
-        localStorage.setItem(lsKey, cm.getValue());
-        $("#save_message").text("Code saved " + new Date().toTimeString());
-        returnObj.trigger("change");
-    };
-
-    var existingCode = localStorage.getItem(lsKey);
-    if(existingCode) {
-        cm.setValue(existingCode);
-    } else {
-        reset();
+        // Whenever the elevator is idle (has no more queued destinations) ...
+        elevator.on("idle", function() {
+            // let's go to all the floors (or did we forget one?)
+            elevator.goToFloor(0);
+            elevator.goToFloor(1);
+        });
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
     }
-
-    $("#button_save").click(function() {
-        saveCode();
-        cm.focus();
-    });
-
-    $("#button_reset").click(function() {
-        if(confirm("Do you really want to reset to the default implementation?")) {
-            localStorage.setItem("develevateBackupCode", cm.getValue());
-            reset();
-        }
-        cm.focus();
-    });
-
-    $("#button_resetundo").click(function() {
-        if(confirm("Do you want to bring back the code as before the last reset?")) {
-            cm.setValue(localStorage.getItem("develevateBackupCode") || "");
-        }
-        cm.focus();
-    });
-
-    var returnObj = riot.observable({});
-    var autoSaver = _.debounce(saveCode, 1000);
-    cm.on("change", function() {
-        autoSaver();
-    });
-
-    returnObj.getCodeObj = function() {
-        console.log("Getting code...");
-        var code = cm.getValue();
-        var obj;
-        try {
-            obj = getCodeObjFromCode(code);
-            returnObj.trigger("code_success");
-        } catch(e) {
-            returnObj.trigger("usercode_error", e);
-            return null;
-        }
-        return obj;
-    };
-    returnObj.setCode = function(code) {
-        cm.setValue(code);
-    };
-    returnObj.getCode = function() {
-        return cm.getValue();
-    }
-    returnObj.setDevTestCode = function() {
-        cm.setValue($("#devtest-elev-implementation").text().trim());
-    }
-
-    $("#button_apply").click(function() {
-        returnObj.trigger("apply_code");
-    });
-    return returnObj;
 };
-
 
 var createParamsUrl = function(current, overrides) {
     return "#" + _.map(_.merge(current, overrides), function(val, key) {
@@ -120,7 +24,6 @@ var createParamsUrl = function(current, overrides) {
 
 $(function() {
     var tsKey = "elevatorTimeScale";
-    var editor = createEditor();
 
     var params = {};
 
@@ -142,7 +45,6 @@ $(function() {
     app.worldController = createWorldController(1.0 / 60.0);
     app.worldController.on("usercode_error", function(e) {
         console.log("World raised code error", e);
-        editor.trigger("usercode_error", e);
     });
 
     console.log(app.worldController);
@@ -191,34 +93,9 @@ $(function() {
             }
         });
 
-        var codeObj = editor.getCodeObj();
         console.log("Starting...");
         app.worldController.start(app.world, codeObj, window.requestAnimationFrame, autoStart);
     };
-
-    editor.on("apply_code", function() {
-        app.startChallenge(app.currentChallengeIndex, true);
-    });
-    editor.on("code_success", function() {
-        presentCodeStatus($codestatus, codeStatusTempl);
-    });
-    editor.on("usercode_error", function(error) {
-        presentCodeStatus($codestatus, codeStatusTempl, error);
-    });
-    editor.on("change", function() {
-        $("#fitness_message").addClass("faded");
-        var codeStr = editor.getCode();
-        // fitnessSuite(codeStr, true, function(results) {
-        //     var message = "";
-        //     if(!results.error) {
-        //         message = "Fitness avg wait times: " + _.map(results, function(r){ return r.options.description + ": " + r.result.avgWaitTime.toPrecision(3) + "s" }).join("&nbsp&nbsp&nbsp");
-        //     } else {
-        //         message = "Could not compute fitness due to error: " + results.error;
-        //     }
-        //     $("#fitness_message").html(message).removeClass("faded");
-        // });
-    });
-    editor.trigger("change");
 
     riot.route(function(path) {
         params = _.reduce(path.split(","), function(result, p) {
